5e4d39fa8eb6249e0ff2b61a630fb6154e596a83
MaicolSSB <maicolsanbed@gmail.com>
2026-09-11 12:20:23 -0500

    chore: configuracion inicial del proyecto

---
+++ votacion.py
@@ -0,0 +1,6 @@
+# Sistema de Votación - Proyecto Integrador
+
+votos = {} # Diccionario para guardar los votos: {votante: candidato}
+
+# Aquí iremos agregando las funciones en las diferentes ramas
+
